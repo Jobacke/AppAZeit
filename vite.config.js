6e9afbf9f1c98a5ef2d7e47e5a6e6b521485d551
Johannes Backhaus <johannes@Mac.fritz.box>
2026-01-28 17:46:15 +0100

    Fix: Rename icons to v2 to enforce cache busting

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         VitePWA({
             registerType: 'autoUpdate',
             manifestFilename: 'manifest.json',
-            includeAssets: ['icon-192.png', 'icon-512.png'],
+            includeAssets: ['icon-192-v2.png', 'icon-512-v2.png'],
             manifest: {
                 name: "App a' Zeit",
                 short_name: "App a' Zeit",
@@ -19,12 +19,12 @@ export default defineConfig({
                 display: 'standalone',
                 icons: [
                     {
-                        src: 'icon-192.png',
+                        src: 'icon-192-v2.png',
                         sizes: '192x192',
                         type: 'image/png'
                     },
                     {
-                        src: 'icon-512.png',
+                        src: 'icon-512-v2.png',
                         sizes: '512x512',
                         type: 'image/png'
                     }
